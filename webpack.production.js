c65ae235c7130e78b4273ad9c08e5459c7d5ba85
TeslaOwner <87253308+TeslaOwner@users.noreply.github.com>
2025-10-23 19:36:54 -0400

    Revert this change since it's used for npm builds

--- webpack.production.js
+++ webpack.production.js
@@ -1,34 +1,27 @@
 const path = require("path");
 const webpack = require("webpack");
 const { merge } = require('webpack-merge');
-const CopyPlugin = require("copy-webpack-plugin");
 
 const common = require('./webpack.common.js');
 
 // used for production builds
 module.exports = merge(common, {
   mode: "production",
-  entry: "./sample/sample.ts",
+  entry: "./src/index.ts",
   output: {
-    filename: "sample.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    library: {
+      name: 'OldSchoolSDK',
+      type: 'umd',
+    },
+    umdNamedDefine: true,
+    path: path.resolve(__dirname, "_bundles"),
     publicPath: '', // workaround: https://github.com/cypress-io/cypress/issues/18435
   },
   optimization: {
     minimize: false
   },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: `index.html`, to: "", context: `sample/` },
-        { from: `assets/fonts/*.woff`, to: "", context: `src/` },
-        { from: `assets/fonts/*.woff2`, to: "", context: `src/` },
-      ],
-    }),
-    new webpack.EnvironmentPlugin({
-      BUILD_DATE: process.env.BUILD_DATE || new Date().toISOString(),
-      COMMIT_REF: process.env.COMMIT_REF || "local",
-      DEPLOY_URL: process.env.DEPLOY_URL || "http://localhost:8000/"
-    }),
-  ],
+  externals: {
+    three: 'three'
+  },
 });
