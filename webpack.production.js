a77ecc615c82c7c604ebda397a200304becef12b
TeslaOwner <87253308+TeslaOwner@users.noreply.github.com>
2025-10-23 19:35:10 -0400

    Trying to get Netlify to work.
    Updated entry point and output filename for production build. Added CopyPlugin to handle asset copying.

--- webpack.production.js
+++ webpack.production.js
@@ -1,27 +1,34 @@
 const path = require("path");
 const webpack = require("webpack");
 const { merge } = require('webpack-merge');
+const CopyPlugin = require("copy-webpack-plugin");
 
 const common = require('./webpack.common.js');
 
 // used for production builds
 module.exports = merge(common, {
   mode: "production",
-  entry: "./src/index.ts",
+  entry: "./sample/sample.ts",
   output: {
-    filename: "main.js",
-    library: {
-      name: 'OldSchoolSDK',
-      type: 'umd',
-    },
-    umdNamedDefine: true,
-    path: path.resolve(__dirname, "_bundles"),
+    filename: "sample.js",
+    path: path.resolve(__dirname, "dist"),
     publicPath: '', // workaround: https://github.com/cypress-io/cypress/issues/18435
   },
   optimization: {
     minimize: false
   },
-  externals: {
-    three: 'three'
-  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: `index.html`, to: "", context: `sample/` },
+        { from: `assets/fonts/*.woff`, to: "", context: `src/` },
+        { from: `assets/fonts/*.woff2`, to: "", context: `src/` },
+      ],
+    }),
+    new webpack.EnvironmentPlugin({
+      BUILD_DATE: process.env.BUILD_DATE || new Date().toISOString(),
+      COMMIT_REF: process.env.COMMIT_REF || "local",
+      DEPLOY_URL: process.env.DEPLOY_URL || "http://localhost:8000/"
+    }),
+  ],
 });
