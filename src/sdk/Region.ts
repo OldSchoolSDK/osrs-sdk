bcd4833af8a2b271b1d869d4fcb5eccad6a3a3a0
supalosa <supalosa@supalosa.com>
2026-05-10 18:50:03 +1000

    Refine region renderable lifecycle handling

--- src/sdk/Region.ts
+++ src/sdk/Region.ts
@@ -5,6 +5,7 @@ import type { Entity } from "./Entity";
 import type { Item } from "./Item";
 import type { Mob } from "./Mob";
 import type { Player } from "./Player";
+import type { Renderable } from "./Renderable";
 import { Settings } from "./Settings";
 import type { World } from "./World";
 import type { Projectile } from "./weapons/Projectile";
@@ -89,7 +90,6 @@ export abstract class Region {
   }
 
   removeEntity(entity: Entity) {
-    entity.dying = 0;
     remove(this.entities, entity);
   }
 
@@ -103,8 +103,8 @@ export abstract class Region {
   }
 
   removeMob(mob: Mob) {
-    mob.dying = 0;
     remove(this.mobs, mob);
+    remove(this.newMobs, mob);
   }
 
   removePlayer(player: Player) {
@@ -223,5 +223,13 @@ export abstract class Region {
     await Promise.all(this.mobs.map((mob) => mob.preload()));
     await Promise.all(this.newMobs.map((mob) => mob.preload()));
     await Promise.all(this.players.map((players) => players.preload()));
+  }
+
+  getRenderables(): Renderable[] {
+    const units = [...this.players, ...this.mobs, ...this.newMobs];
+    const incomingProjectiles: Renderable[] = [];
+    units.forEach((unit) => incomingProjectiles.push(...unit.incomingProjectiles));
+
+    return [...this.entities, ...units, ...this.projectiles, ...incomingProjectiles];
   }
 }
